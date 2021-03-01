f2d43c850a63c26af62fb4cc882aa8a8f65e30e9
Deeksha <42764871+Deekshadeechu@users.noreply.github.com>
2021-03-01 13:01:04 +0530

    Add files via upload

---
+++ student.py
@@ -0,0 +1,14 @@
+class student:
+
+    def __init__(self,maths,science,social):
+        self.maths = maths
+        self.science = science
+        self.social = social
+
+    def get_highest_marks_maths(self):
+        print('get_highest_marks')
+        return 67
+
+    def get_highest_marks_sci(self):
+        print('get_highest_marks')
+        return 67
